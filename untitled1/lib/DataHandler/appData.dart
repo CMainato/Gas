cdd093bb5891aa7f8af7f440b017b737b0f62ee9
CMainato <christian.mainato@epn.edu.ec>
2021-11-12 16:21:55 -0500

    historial

--- untitled1/lib/DataHandler/appData.dart
+++ untitled1/lib/DataHandler/appData.dart
@@ -1,10 +1,16 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:untitled1/Models/address.dart';
-
+import 'package:untitled1/Models/history.dart';
 
 class AppData extends ChangeNotifier
 {
   Address pickUpLocation , dropOffLocation;
+  String earnings = "0";
+  int countTrips = 0;
+  List<String> tripHistoryKeys = [];
+  List<History> tripHistoryDataList =[];
+  //List<History> tripHistoryDataList = [];
 
   void updatePickUpLocationAddress(Address pickUpAddress)
   {
@@ -17,5 +23,30 @@ class AppData extends ChangeNotifier
     dropOffLocation = dropOffAddres;;
     notifyListeners();
 
+  }
+
+  //history
+  void updateEarnings(String updatedEarnings)
+  {
+    earnings = updatedEarnings;
+    notifyListeners();
+  }
+
+  void updateTripsCounter(int tripCounter)
+  {
+    countTrips = tripCounter;
+    notifyListeners();
+  }
+
+  void updateTripKeys(List<String> newKeys)
+  {
+    tripHistoryKeys = newKeys;
+    notifyListeners();
+  }
+
+  void updateTripHistoryData(History eachHistory)
+  {
+    tripHistoryDataList.add(eachHistory);
+    notifyListeners();
   }
 }
